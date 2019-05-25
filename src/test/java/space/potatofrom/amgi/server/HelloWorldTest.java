887e63045110e3a8220c1db2e76a913997898c56
Kevin Liu <kevin@potatofrom.space>
2019-05-24 23:57:43 -0400

    Add example test

---
+++ src/test/java/space/potatofrom/amgi/server/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package space.potatofrom.amgi.server;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void test() {
+        System.out.println("It works!");
+    }
+}

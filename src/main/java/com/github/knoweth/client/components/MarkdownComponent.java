dd1b8a525094ff9e573532a54630acd844f99334
Kevin Liu <kevin@potatofrom.space>
2019-08-23 15:47:49 -0400

    Implement Markdown component

--- src/main/java/com/github/knoweth/client/components/MarkdownComponent.java
+++ src/main/java/com/github/knoweth/client/components/MarkdownComponent.java
@@ -3,26 +3,22 @@ package com.github.knoweth.client.components;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
-import org.teavm.flavour.templates.BindAttribute;
-import org.teavm.flavour.templates.BindElement;
-import org.teavm.flavour.templates.BindTemplate;
-import org.teavm.flavour.templates.Slot;
+import org.teavm.flavour.templates.*;
 import org.teavm.flavour.widgets.AbstractWidget;
+import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.html.HTMLElement;
 
 import java.util.function.Supplier;
 
-/**
- * Component that renders its content as markdown.
- */
-@BindTemplate("templates/components/markdown.html")
 @BindElement(name = "markdown")
+@IgnoreContent
 public class MarkdownComponent extends AbstractWidget {
     private Supplier<String> contentSupplier;
+    private NodeHolder renderedSlot;
 
     public MarkdownComponent(Slot slot) {
         super(slot);
     }
-
 
     /*
      * Tell template engine that we want our component to support 'content' attribute.
@@ -35,11 +31,21 @@ public class MarkdownComponent extends AbstractWidget {
     /*
      * Expose rendered content to `components/markdown.html`.
      */
-    public String getRendered() {
+    @Override
+    public void render() {
+        if (renderedSlot != null) {
+            renderedSlot.delete();
+        }
+
         String raw = contentSupplier.get();
         Parser parser = Parser.builder().build();
         Node document = parser.parse(raw);
         HtmlRenderer render = HtmlRenderer.builder().build();
-        return render.render(document);
+        String html = render.render(document);
+        HTMLElement node = Window.current().getDocument().createElement("div");
+        node.setInnerHTML(html);
+
+        renderedSlot = new NodeHolder(node);
+        getSlot().append(renderedSlot);
     }
 }
